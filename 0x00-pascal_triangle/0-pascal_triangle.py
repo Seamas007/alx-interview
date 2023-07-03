f5ef230dde9301b4be54beffea4820f252f8a6e0
Adefemi Oreyomi <olufy007@gmail.com>
2023-07-03 23:05:56 +0100

    My commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
+"""
+Print Pascal's Triangle
+"""
+
 
 def pascal_triangle(n):
-    # Check if n is less than or equal to 0
-    if n <= 0:
-        return []  # Return an empty list if n is invalid
-    
-    triangle = [[1]]  # Initialize the triangle with the first row containing a single 1
-    
-    # Generate the subsequent rows of the triangle
-    for i in range(1, n):
-        row = [1]  # Start each row with a 1
-        
-        # Calculate each element of the current row
-        for j in range(1, i):
-            # Each element is the sum of the corresponding elements from the previous row
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        
-        row.append(1)  # End each row with a 1
-        triangle.append(row)  # Append the current row to the triangle
-    
-    return triangle  # Return the completed triangle
+    """returns Pascal's triangle in a list of list"""
+    my_list = []
+    if (n <= 0):
+        return my_list
+    my_list.append([1])
+    for i in range(n - 1):
+        my_list.append([1] + [my_list[i][a] + my_list[i][a + 1]
+                              for a in range(len(my_list[i]) - 1)] + [1])
+    return my_list
